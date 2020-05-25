8dc2846a3383b47895bab10e8d69083d71818fb8
Anirudh-030307 <65300694+Anirudh-030307@users.noreply.github.com>
2020-05-25 18:59:27 +0530

    Add files via upload

---
+++ collided.js
@@ -0,0 +1,14 @@
+function collide(bullet,wall) {
+   
+    bulletRightEdge=bullet.x+bullet.width;
+    wallLeftEdge=wall.x;
+
+    if (bulletRightEdge>=wallLeftEdge) {
+        
+        return true
+
+    }
+
+    return false;
+
+}
